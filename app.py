fb32661cb7ca5a238b5aa0f833a4bc26645aa362
Miguelcleyva <micossioleyva@gmail.com>
2025-07-02 10:35:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title="Seguimiento de Taller", layout="wide")
 st.title("📋 Seguimiento de Vehículos en Taller")
 
 st.markdown("""
-Esta app permite registrar, controlar y visualizar el avance de vehículos en un taller de planchado y pintura. 
+Esta app permite registrar, controlar y visualizar el avance de vehículos en un taller de planchado y pintura.  
 Incluye integración con Google Sheets y Drive, generación de PDF y panel de KPIs.
 """)
